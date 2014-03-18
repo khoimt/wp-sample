39c82042da1abdc85fccff378c115c49d86c7e1f
khoimt <khoimt@email>
2014-03-18 18:46:22 +0700

    update ConstestSubmitForm

--- wp-content/plugins/ConstestSubmitForm/submit.php
+++ wp-content/plugins/ConstestSubmitForm/submit.php
@@ -1,6 +1,19 @@
 <?php
 
-if (!session_id()) {
-    session_start();
+require_once(realpath(dirname(__FILE__) . '/../../../wp-load.php'));
+
+if (!is_user_logged_in()
+        || !array_key_exists('salt', $_POST)
+        || !array_key_exists($_POST['salt'], $_SESSION['submit_salt'])) {
+    wp_redirect(get_bloginfo('url') . '/404');
 }
+
+unset($_SESSION['submit_salt'][$_POST['salt']]);
+
+require_once realpath(dirname(__FILE__) . '/uploader.php');
+$ob = new ProblemUploader();
+$ob->handleUploading();
+
+
+
 
--- wp-content/plugins/ConstestSubmitForm/submitform.php
+++ wp-content/plugins/ConstestSubmitForm/submitform.php
@@ -1,21 +1,25 @@
 <?php
-    $salt = new stdClass();
-    $salt->content = md5('md5_' . rand(0, 1000000));
-    $salt->createdTime = TIME_NOW;
-    $_SESSION['submit_salt'] = $salt;
+    $salt = md5('md5_' . rand(0, 1000000));
+    $_SESSION['submit_salt'][$salt] = TIME_NOW;
 ?>
 <div class="clear"></div>
 <div id="problem-submit-block" class="problem-submit-block">
-    <form action="<?php echo plugin_dir_url(__FILE__) ?>submit.php" method="POST" enctype="multipart-forms/data">
-        <div class='left-block'>
-            <input name="problems[0]" type="file" />
-            <div class="clear"></div>
-            <input name="problems[1]" type="file" />
-            <div class="clear"></div>
-            <input type='hidden' name='salt' value='<?php echo $salt->content ?>' />
+    <?php if (is_user_logged_in()) : ?>
+        <form action="<?php echo plugin_dir_url(__FILE__) ?>submit.php" method="POST" enctype="multipart/form-data">
+            <div class='left-block'>
+                <input name="problems[]" type="file" />
+                <div class="clear"></div>
+                <input name="problems[]" type="file" />
+                <div class="clear"></div>
+                <input type='hidden' name='salt' value='<?php echo $salt ?>' />
+            </div>
+            <div class='right-block'>
+                <input type="submit" value='Nộp Bài'/>
+            </div>
+        </form>
+    <?php else: ?>
+        <div class="msg">
+            You must login to submit your result
         </div>
-        <div class='right-block'>
-            <input type="submit" value='Nộp Bài'/>
-        </div>
-    </form>
+    <?php endif ?>
 </div>
